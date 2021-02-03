eaa190d1286cda7d7dba981348f72e6d901111d2
BoraKurucu <borakurucu11@gmail.com>
2021-02-04 01:51:26 +0300

    finished game

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 
     Rigidbody rig;
     AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+    [SerializeField] ParticleSystem mainEngineP;
+    [SerializeField] ParticleSystem successP;
+    [SerializeField] ParticleSystem deathP;
+
+
+    enum State { Alive,Dying,Transcending};
+    State state = State.Alive;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,29 +32,89 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
-    }
-    private void ProcessInput()
-    {
-        if(Input.GetKey(KeyCode.Space))
+        if (state == State.Alive)
         {
-            rig.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            Thrust();
+            Rotate();
+        }
+    }
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
         }
         else
         {
             audioSource.Stop();
+            mainEngineP.Stop();
         }
+
+    }
+
+    private void ApplyThrust()
+    {
+        rig.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineP.Play();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(state != State.Alive)
+        {
+            return;
+        }
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Friendly");
+                break;
+            case "Finish":
+                state = State.Transcending;
+                audioSource.Stop();
+                audioSource.PlayOneShot(success);
+                Invoke("LoadNextScene", 1f) ;
+                successP.Play();
+                print("Hit Finish");
+                break;
+            default:
+                print("Dead");
+                state = State.Dying;
+                audioSource.Stop();
+                audioSource.PlayOneShot(death);
+                deathP.Play();
+                Invoke("LoadFirstLevel", 1f);
+                break;
+
+        }
+    }
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void Rotate()
+    {
+        rig.freezeRotation = true;
+       
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+       
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+        rig.freezeRotation = false;
+
     }
 }
